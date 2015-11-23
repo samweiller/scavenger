3b04dd36d91f63bb05d5498f3befde27f9f31143
Sam Weiller <sam.weiller@gmail.com>
2015-11-23 11:06:36 -0500

    Added more viewControllers

---
+++ Scavenger/LeaderboardViewController.swift
@@ -0,0 +1,25 @@
+//
+//  LeaderboardViewController.swift
+//  Scavenger
+//
+//  Created by Sam Weiller on 11/23/15.
+//  Copyright © 2015 saweiller. All rights reserved.
+//
+
+import UIKit
+
+class LeaderboardViewController: UIViewController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+    
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    
+}
+
---
+++ Scavenger/welcomeViewController.swift
@@ -0,0 +1,24 @@
+//
+//  welcomeViewController.swift
+//  Scavenger
+//
+//  Created by Sam Weiller on 11/23/15.
+//  Copyright © 2015 saweiller. All rights reserved.
+//
+
+import UIKit
+
+class WelcomeViewController: UIViewController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+    
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    
+}
